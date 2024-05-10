110103871e2d6260cabd411b3a4b6128649bf797
Nathan <nhnwong@gmail.com>
2024-05-10 00:19:53 -0400

    Added a visual nesting effect

--- Scripts/code_editor.gd
+++ Scripts/code_editor.gd
@@ -9,12 +9,36 @@ var outliner
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-func movement_changes(index):
-	canvas[index].move_to_front()
-	canvas[index].scale = Vector2(2, 1)
-	canvas[index].get_node("RichTextLabel").queue_free()
-	for n in canvas[index].get_children():
+func determine_nest(this_object, bound_one, bound_two, bound_three, bound_four):
+	var distance = bound_one - 394
+	var size = bound_two - bound_one
+	var mouse_co = get_viewport().get_mouse_position()
+	if abs(mouse_co.y - this_object.position.y) <= 5:
+		if mouse_co.x >= bound_one and mouse_co.x <= bound_two:
+			if str(this_object.get_children()[len(this_object.get_children()) - 1].get_name())[0] == '_' and this_object.get_children()[len(this_object.get_children()) - 1].position == Vector2(25, 0):
+#use bound_one and bound_two
+				determine_nest(this_object.get_children()[len(this_object.get_children()) - 1], bound_one, bound_two, bound_three, bound_four)
+			else:	
+				this_object.add_child(clone.duplicate())
+				this_object.get_children()[len(this_object.get_children()) - 1].position = Vector2(25, 0)
+				movement_changes(this_object.get_children()[len(this_object.get_children()) - 1])
+				this_object.get_children()[len(this_object.get_children()) - 1].scale = Vector2(0.5, 0.5)
+		elif mouse_co.x >= bound_three and mouse_co.x <= bound_four:
+			if str(this_object.get_children()[len(this_object.get_children()) - 1].get_name())[0] == '_' and this_object.get_children()[len(this_object.get_children()) - 1].position == Vector2(25, 0):
+#use bound_three and bound_four
+				determine_nest(this_object.get_children()[len(this_object.get_children()) - 1], bound_one, bound_two, bound_three, bound_four)
+			else:
+				this_object.add_child(clone.duplicate())
+				this_object.get_children()[len(this_object.get_children()) - 1].position = Vector2(-25, 0)
+				movement_changes(this_object.get_children()[len(this_object.get_children()) - 1])
+				this_object.get_children()[len(this_object.get_children()) - 1].scale = Vector2(0.5, 0.5)
+		return false
+	return true
+func movement_changes(moved_object):
+	moved_object.move_to_front()
+	moved_object.scale = Vector2(2, 1)
+	moved_object.get_node("RichTextLabel").queue_free()
+	for n in moved_object.get_children():
 		n.scale = Vector2(0.5, 1)
 		if n.get_name() == "Equal":
 			n.scale = Vector2(0.05, 0.1)
@@ -42,42 +66,48 @@ func _unhandled_input(event):
 		else:
 			if mouse_index != -1 and (mouse_co.y >= 80 and mouse_co.y <= 650) and (mouse_co.x >= 204 and mouse_co.x <= 584):
 				var first_index = 365 - 45 * floor((len(canvas) + 1) / 2) - 15 * ((len(canvas) + 1) % 2)
-				if len(canvas) == 0 or mouse_co.y > canvas[len(canvas) - 1].position.y:
-					canvas.append(clone.duplicate())
-					get_parent().add_child(canvas[len(canvas) - 1])
-					canvas[len(canvas) - 1].position = Vector2(394, 0)
-					movement_changes(len(canvas) - 1)
-				elif mouse_co.y < canvas[0].position.y:
-					var temp = []
-					for item in canvas:
-						temp.append(item)
-					canvas.clear()
-					canvas.append(clone.duplicate())
-					for item in temp:
-						canvas.append(item)
-					temp.clear()
-					get_parent().add_child(canvas[0])
-					canvas[0].position = Vector2(394, 0)
-					movement_changes(0)
-				else:
-					var temp = []
-					var index = len(canvas)
-					for i in range (highlighted_index, index):
-						temp.append(canvas[highlighted_index])
-						canvas.remove_at(highlighted_index)
-					canvas.append(clone.duplicate())
-					get_parent().add_child(canvas[highlighted_index])
-					if outliner:
-						canvas[highlighted_index].position = Vector2(canvas[highlighted_index - 1].position.x + 41, 0)
+				var basis = true
+				for j in range (0, len(canvas)):
+					if determine_nest(canvas[j], 410, 477, 310, 377) == false:
+						basis = false
+						break
+				if basis:
+					if len(canvas) == 0 or mouse_co.y > canvas[len(canvas) - 1].position.y:
+						canvas.append(clone.duplicate())
+						get_parent().add_child(canvas[len(canvas) - 1])
+						canvas[len(canvas) - 1].position = Vector2(394, 0)
+						movement_changes(canvas[len(canvas) - 1])
+					elif mouse_co.y < canvas[0].position.y:
+						var temp = []
+						for item in canvas:
+							temp.append(item)
+						canvas.clear()
+						canvas.append(clone.duplicate())
+						for item in temp:
+							canvas.append(item)
+						temp.clear()
+						get_parent().add_child(canvas[0])
+						canvas[0].position = Vector2(394, 0)
+						movement_changes(canvas[0])
 					else:
-						canvas[highlighted_index].position = Vector2(394, 0)
-					movement_changes(highlighted_index)
-					for item in temp:
-						canvas.append(item)
-					temp.clear()
-				if len(canvas) <= 18:
-					for i in range (0, len(canvas)):
-						canvas[i].position = Vector2(canvas[i].position.x, first_index + i * 45)
+						var temp = []
+						var index = len(canvas)
+						for i in range (highlighted_index, index):
+							temp.append(canvas[highlighted_index])
+							canvas.remove_at(highlighted_index)
+						canvas.append(clone.duplicate())
+						get_parent().add_child(canvas[highlighted_index])
+						if outliner:
+							canvas[highlighted_index].position = Vector2(canvas[highlighted_index - 1].position.x + 41, 0)
+						else:
+							canvas[highlighted_index].position = Vector2(394, 0)
+						movement_changes(canvas[highlighted_index])
+						for item in temp:
+							canvas.append(item)
+						temp.clear()
+					if len(canvas) <= 18:
+						for i in range (0, len(canvas)):
+							canvas[i].position = Vector2(canvas[i].position.x, first_index + i * 45)
 			if clone != null:
 				clone.queue_free()
 				clone = null
@@ -98,12 +128,15 @@ func _process(delta):
 		if len(canvas) > 0 and (mouse_co.y >= 80 and mouse_co.y <= 650) and (mouse_co.x >= 204 and mouse_co.x <= 584):
 			var basis = true
 			for j in range (0, len(canvas)):
-				if abs(mouse_co.y - canvas[j].position.y) <= 5:
+				if basis and abs(mouse_co.y - canvas[j].position.y) <= 5:
 					if mouse_co.x >= 410 and mouse_co.x <= 477:
 						canvas[j].get_node("TheBox").get_node("LineEdit").text = "NEST"
 					elif mouse_co.x >= 310 and mouse_co.x <= 377:
 						canvas[j].get_node("TheBox2").get_node("LineEdit").text = "NEST"
-					basis = false				
+					basis = false
+				else:
+					canvas[j].get_node("TheBox").get_node("LineEdit").text = canvas[j].get_node("TheBox").get_node("LineEdit").placeholder_text
+					canvas[j].get_node("TheBox2").get_node("LineEdit").text = canvas[j].get_node("TheBox2").get_node("LineEdit").placeholder_text
 			var first_index = 365 - 45 * floor((len(canvas) + 1) / 2) - 15 * ((len(canvas) + 1) % 2)
 			if basis and mouse_co.y > canvas[0].position.y and mouse_co.y < canvas[len(canvas) - 1].position.y:
 				highlighted_index = floor((mouse_co.y - canvas[0].position.y) / 45) + 1
